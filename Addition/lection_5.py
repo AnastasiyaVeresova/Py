99daa8f0623ae0a01ae99a1e1073cbca84ffa6fd
AnastasiyaVeresova <140641844+AnastasiyaVeresova@users.noreply.github.com>
2023-11-15 15:31:52 +0300

    Update lection_5.py

--- Addition/lection_5.py
+++ Addition/lection_5.py
@@ -3,7 +3,6 @@ def f(x):
 a = f
 print(f(5))
 print(a(5))
-
 ____________________________________________________________________миникалькулятор
 
 def calc1(a, b):
@@ -22,3 +21,32 @@ math(calc2, 5, 7)
 # calc1 = lambda a,b: a + b
     
 math(lambda a,b: a + b, 5, 7)
+
+___________________________________________________
+data = [1, 2, 3, 5, 8, 15, 23, 38]
+result = []
+
+for i in data:
+    if i % 2 == 0:
+        result.append((i, i**2))
+
+print(result)
+
+# [(2, 4), (8, 64), (38, 1444)]
+________________________________________________lambda function
+
+def select(f, col):
+    return [f(x) for x in col]
+    
+def where(f, col):
+    return [x for x in col if f(x)]
+    
+data = [1, 2, 3, 5, 8, 15, 23, 38]
+res = select(int, data)
+print(res)
+res = where(lambda x: x % 2 == 0, res)
+print(res)
+
+#[1, 2, 3, 5, 8, 15, 23, 38]
+#[2, 8, 38]
+________________________________________________________
