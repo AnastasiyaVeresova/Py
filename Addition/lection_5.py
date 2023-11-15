e8827ce1f6bcec677cc20e2644ddfac1416b1a59
AnastasiyaVeresova <140641844+AnastasiyaVeresova@users.noreply.github.com>
2023-11-15 15:33:13 +0300

    Update lection_5.py

--- Addition/lection_5.py
+++ Addition/lection_5.py
@@ -50,3 +50,39 @@ print(res)
 #[1, 2, 3, 5, 8, 15, 23, 38]
 #[2, 8, 38]
 ________________________________________________________
+
+
+def select(f, col):
+    return [f(x) for x in col]
+    
+def where(f, col):
+    return [x for x in col if f(x)]
+    
+data = [1, 2, 3, 5, 8, 15, 23, 38]
+res = select(int, data)
+print(res)
+res = where(lambda x: x % 2 == 0, res)
+print(res)
+res = list(select(lambda x: (x, x**2), res))
+print(res)
+
+# [1, 2, 3, 5, 8, 15, 23, 38]
+# [2, 8, 38]
+# [(2, 4), (8, 64), (38, 1444)]
+
+-------------------------------------------------
+
+def where(f, col):
+    return [x for x in col if f(x)]
+    
+data = [1, 2, 3, 5, 8, 15, 23, 38]
+res = where(int, data)
+print(res)
+res = where(lambda x: x % 2 == 0, res)
+print(res)
+res = list(map(lambda x: (x, x**2), res))
+print(res)
+
+# [1, 2, 3, 5, 8, 15, 23, 38]
+# [2, 8, 38]
+# [(2, 4), (8, 64), (38, 1444)]
