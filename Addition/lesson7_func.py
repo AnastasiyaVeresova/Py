1ce1ab5f82dfd1e3db0b2499e62957240cc5a19f
AnastasiyaVeresova <140641844+AnastasiyaVeresova@users.noreply.github.com>
2023-11-16 13:24:50 +0300

    Update lesson7_func.py

--- Addition/lesson7_func.py
+++ Addition/lesson7_func.py
@@ -5,11 +5,21 @@ def byebye(name):
     return name + ", bye bye"
 
 def create_phrase(func):
-    name = input("Введите свое имя ")
-    return func(name)
+    name = input("Введите свое имя ") #1
+    return func(name)     # 3
 
-print(create_phrase(hello))
+def create_two_phrase(funcs):
+    name = input("Введите свое имя ") #1
+    res = ""
+    for func in funcs:
+        res += func(name) +"\n"
+    return res     # 3
 
+# print(create_phrase(hello))
+# print(create_phrase(byebye))
+# funcs = (hello, byebye)
+# print(create_two_phrase(funcs))
+_____________________________________________________________________________
 # Мы можем вернуть функцию внутри функции. Рассмотрим создание
 # функции, позволяющей вычислить любое число в любой степени.
 # Сделаем функцию calc_power, которая на входе принимает степень, а
@@ -19,3 +29,11 @@ def calc_power(degree):
     def power(num):
         return num ** degree
     return power
+
+# print(calc_power(3) (2) )
+
+cube = calc_power(3)
+square = calc_power(2)
+
+print(cube(4))
+print(square(9))
