e982030b71438f7ac4fba2503fce37664652b687
AnastasiyaVeresova <140641844+AnastasiyaVeresova@users.noreply.github.com>
2023-11-16 17:04:44 +0300

    Update 36.py

--- Lesson5/36.py
+++ Lesson5/36.py
@@ -4,3 +4,11 @@
 # Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
 # Примечание: бинарной операцией называется любая операция,
 # у которой ровно два аргумента, как, например, у операции умножения. 
+# ОШИБКА! Размерности таблицы должны быть больше 2!.
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    res = [[operation(i, j) for i in range(1, num_columns + 1)] for j in range(1, num_rows + 1)]
+    for i in res:
+        print(*[f"{x}" for x in i])
+
+print_operation_table(lambda x, y: x * y)
