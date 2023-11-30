df0043f980bdc7278b94fac3840a75802c9e5ace
AV <nastulua@yandex.ru>
2023-11-30 13:08:16 +0300

    Добавлено решение по df

---
+++ DataFrame/df.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+
+one_hot_dict = {}
+
+for value in data['whoAmI'].unique():
+    one_hot_dict[value] = [1 if x == value else 0 for x in data['whoAmI']]
+
+one_hot_data = pd.DataFrame(one_hot_dict)
+one_hot_data.head()
+print(one_hot_data)
